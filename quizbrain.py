dc0f1f0e9f87dbc135a4b1efd92a7b4e3a1566f0
Junaid Hasan <97498014+junaidhas@users.noreply.github.com>
2023-10-12 00:06:18 -0400

    Update quizbrain.py

--- quizbrain.py
+++ quizbrain.py
@@ -5,8 +5,8 @@ class QuizBrain:
     def __init__(self,q_list):
         self.question_number = 0
         self.question_list = q_list
-        self.score = 0
-        self.current_question = None
+        self.score=0
+        self.current_question =None
 
     def still_has_questions(self):
         return self.question_number<len(self.question_list)
@@ -17,11 +17,8 @@ class QuizBrain:
         q_text =html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
 
-        # user_answer=input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        # self.check_answer(user_answer,current_question.answer)
-
     def check_answer(self,user_answer):
-        correct_answer =self.current_question.answer
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             print("you got it right!!")
             self.score += 1
@@ -30,6 +27,6 @@ class QuizBrain:
         else:
             print("you got it wrong")
             return False
-        
-        
-        
+        print(f"the correct answer is {correct_answer} ")
+        print(f"your score is {self.score}/{self.question_number}")
+        print("\n")
